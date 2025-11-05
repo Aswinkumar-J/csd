9074972e53ad5fff0d1e60b56d5795f5a6bd6d75
Aswinkumar-J <230701044@rajalakshmi.edu.in>
2025-11-05 06:00:29 +0530

    Create Home component for Medicine Tracker App

---
+++ Home.jsx
@@ -0,0 +1,13 @@
+// Home.jsx
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h2>Welcome to the Medicine Tracker App</h2>
+      <p>Manage your medicines and health schedule efficiently.</p>
+    </div>
+  );
+};
+
+export default Home;
