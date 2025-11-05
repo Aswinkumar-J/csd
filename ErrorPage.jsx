323241f49f6a3c207fba52ed262b0525bbbf3d19
Aswinkumar-J <230701044@rajalakshmi.edu.in>
2025-11-05 05:59:11 +0530

    Add ErrorPage component for error handling

---
+++ ErrorPage.jsx
@@ -0,0 +1,13 @@
+// ErrorPage.jsx
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h2>Something Went Wrong</h2>
+      <p>We're sorry, but an error occurred. Please try again later.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
